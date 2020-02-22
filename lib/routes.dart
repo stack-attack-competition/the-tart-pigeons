4fa7d3259de0c2b26c551614fbfd38a6d117fe69
daniel.papilion <daniel.papilion@tekrama.com>
2020-02-22 11:24:23 +0100

    Initialize routing.

---
+++ lib/routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:the_tart_pigeons/screens/home/home.dart';
+
+final routes = <String, WidgetBuilder>{
+  '/home': (BuildContext context) => new HomePage()
+};
